8927ab7cf26902d280d9da944aca2d628d9fcd82
Erons <eronmmer@gmail.com>
2021-03-28 21:52:12 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const { ApolloServer, gql } = require("apollo-server");
+
+const typeDefs = gql`
+	type Query {
+		hello: String!
+	}
+`;
+
+const resolvers = {
+	Query: {
+		hello: () => "hello world",
+	},
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => console.log(`Server started at ${url}`));
