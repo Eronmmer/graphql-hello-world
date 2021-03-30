d3944c9c14f10188da3b6df81be5d78a680975ee
Erons <eronmmer@gmail.com>
2021-03-30 08:50:50 +0100

    types...

---
+++ readme.md
@@ -0,0 +1,10 @@
+# Learning GraphQL
+
+### [GraphQL Schema Language Cheat Sheet](https://github.com/sogko/graphql-schema-language-cheat-sheet)
+
+## Stuff...
+
+- A `!` indicates that a field cannot be null.
+- graphql will cast any value passed to what it expects as specified in the type definitions.
+- `Query` is for stuff you can read. Think of `GET` in rest apis. A `Mutation` on the other hand acts like a PUT, POST & DELETE requests.
+
--- src/index.js
+++ src/index.js
@@ -4,11 +4,56 @@ const typeDefs = gql`
 	type Query {
 		hello: String!
 	}
+
+	type User {
+		id: ID!
+		username: String!
+	}
+
+	type Error {
+		message: String!
+		field: String!
+	}
+
+	type RegisterResponse {
+		user: User
+		errors: [Error]
+	}
+
+	input UserInfo {
+		username: String!
+		password: String!
+		age: Int
+	}
+
+	type Mutation {
+		register(userInfo: UserInfo): RegisterResponse
+	}
 `;
 
 const resolvers = {
 	Query: {
 		hello: () => "hello world",
+	},
+	Mutation: {
+		register: (parent, args, context, info) => ({
+			// args above is how to use an argument in a resolver
+			// The args argument is an object that contains all GraphQL arguments that were provided for the field by the GraphQL operation.
+			user: {
+				id: 23,
+				username: args.userInfo.username,
+			},
+			errors: [
+				{
+					message: "Name is required",
+					field: "name",
+				},
+				{
+					message: "Username is already taken",
+					field: "username",
+				},
+			],
+		}),
 	},
 };
 
