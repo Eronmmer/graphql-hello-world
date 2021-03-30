038623d7ff62310e69ca281d59c801226616a250
Erons <eronmmer@gmail.com>
2021-03-30 15:12:52 +0100

    subscriptions

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const { ApolloServer, gql } = require("apollo-server");
+const { ApolloServer, gql, PubSub } = require("apollo-server");
 
 const typeDefs = gql`
 	type Query {
@@ -29,23 +29,38 @@ const typeDefs = gql`
 	type Mutation {
 		register(userInfo: UserInfo): RegisterResponse
 	}
+
+	type Subscription {
+		newUser: User!
+	}
 `;
 
+const NEW_USER = "NEW_USER";
+
 const resolvers = {
+	Subscription: {
+		newUser: {
+			subscribe: (parent, _, { pubsub }) => pubsub.asyncIterator(NEW_USER),
+		},
+	},
 	Query: {
 		hello: () => "hello world",
 	},
 	Mutation: {
-		register: (parent, args, context, info) => {
-			console.log(context);
-			return {
-				// args above is how to use an argument in a resolver
-				// The args argument is an object that contains all GraphQL arguments that were provided for the field by the GraphQL operation.
+		register: (_, args, { pubsub }) => {
+			// args above is how to use an argument in a resolver
+			const user = {
+				id: 23,
+				username: args.userInfo.username,
+			};
+			// The args argument is an object that contains all GraphQL arguments that were provided for the field by the GraphQL operation.
 
-				user: {
-					id: 23,
-					username: args.userInfo.username,
-				},
+			pubsub.publish(NEW_USER, {
+				newUser: user,
+			});
+			// which the above, the newuser subscription is called each time a user registers
+			return {
+				user,
 				errors: [
 					{
 						message: "Name is required",
@@ -57,9 +72,11 @@ const resolvers = {
 					},
 				],
 			};
-		}
+		},
 	},
 };
+
+const pubsub = new PubSub(); // this can be used to trigger & publish events
 
 const server = new ApolloServer({
 	typeDefs,
@@ -67,6 +84,7 @@ const server = new ApolloServer({
 	context: ({ req, res }) => ({
 		req,
 		res,
+		pubsub,
 	}),
 });
 
